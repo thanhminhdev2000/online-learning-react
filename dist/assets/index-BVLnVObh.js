8d7dd9857a198e38fdef4f3ba9dd1788948171e4
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-11-07 08:14:03 +0700

    Add Loader

---
+++ dist/assets/index-BVLnVObh.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-mui-DDhzcgPb.js";import{I as e}from"./index-Bm6skYEt.js";const a=({length:r})=>t.jsx(t.Fragment,{children:r===0&&t.jsx(e,{sx:{width:"100%",height:200,backgroundColor:"white"},children:"Không tìm thấy dữ liệu"})});export{a as N};
